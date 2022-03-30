7486d39bea8f69fafbe2fc217e44391b2f60f426
confidential-nt <youyoy689@gmail.com>
2022-03-31 01:57:57 +0900

    canal

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,10 @@ import del from "del";
 import sass from "gulp-sass";
 import minify from "gulp-csso";
 import autoprefixer from "gulp-autoprefixer";
+import ghPages from "gulp-gh-pages";
+import htmlMin from "gulp-htmlmin";
+import ws from "gulp-webserver";
+import gimage from "gulp-imagemin";
 
 sass.compiler = require("node-sass");
 
@@ -10,8 +14,17 @@ const routes = {
   css: {
     watch: "src/scss/*",
     src: "src/scss/styles.scss",
-    dest: "dist/css/"
-  }
+    dest: "dist/css/",
+  },
+  html: {
+    watch: "./index.html",
+    src: "./index.html",
+    dest: "dist/",
+  },
+  img: {
+    src: "src/img/*",
+    dest: "dist/img/",
+  },
 };
 
 const styles = () =>
@@ -21,22 +34,40 @@ const styles = () =>
     .pipe(
       autoprefixer({
         flexbox: true,
-        grid: "autoplace"
+        grid: "autoplace",
       })
     )
     .pipe(minify())
     .pipe(gulp.dest(routes.css.dest));
 
+const htmlMinify = () =>
+  gulp.src(routes.html.src).pipe(htmlMin()).pipe(gulp.dest(routes.html.dest));
+
 const watch = () => {
   gulp.watch(routes.css.watch, styles);
+  gulp.watch(routes.html.watch, htmlMinify);
 };
 
-const clean = () => del(["dist/styles.css"]);
+const img = () => {
+  return gulp
+    .src(routes.img.src)
+    .pipe(gimage())
+    .pipe(gulp.dest(routes.img.dest));
+};
 
-const prepare = gulp.series([clean]);
+const clean = () => del(["dist", ".publish"]);
 
-const assets = gulp.series([styles]);
+const webserver = () =>
+  gulp.src("dist").pipe(ws({ livereload: true, open: true }));
 
-const live = gulp.parallel([watch]);
+const gh = () => gulp.src("dist/**/*").pipe(ghPages());
 
-export const dev = gulp.series([prepare, assets, live]);
+const prepare = gulp.series([clean, img]);
+
+const assets = gulp.series([htmlMinify, styles]);
+
+const live = gulp.parallel([webserver, watch]);
+
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([build, gh, clean]);
